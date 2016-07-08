fa7436020104192e84841cd01b9aba29a2178fb3
Iodine <h.olthof@optusnet.com.au>
2016-07-09 09:20:54 +1000

    finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
-A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
@@ -13,11 +14,33 @@ import string
 def reverse(strorint):
     return int(str(strorint)[::-1])
 
-def generate_palindrome(k):
-    return [x for x in itertools.permutations(string.digits, k) if x == x[::-1]]
+
+def palindrome_range(start,stop,step=1):
+    for x in range(start,stop,step):
+        if str(x)==str(x)[::-1]:
+            yield x
+
+
+def is_palindrome(x):
+    if str(x) == str(x)[::-1]:
+        return True
+
 
 def main(k):
-    list = generate_palindrome(k)
+    # for p in palindrome_range(100000, 999999, 1):
+    max_palindrome = 0
+    mx = 0
+    my = 0
+    for x in range(999, 100, -1):
+        for y in range(999, 100, -1):
+            z = x * y
+            if str(z) == str(z)[::-1]:
+                if z > max_palindrome:
+                    max_palindrome = z
+                    mx = x
+                    my = y
+
+    return (max_palindrome, mx, my)
 
 
 if __name__ == '__main__':
@@ -25,6 +48,9 @@ if __name__ == '__main__':
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    cProfile.run('list = main(6)')
-    print(list)
+    cProfile.run('(m, x, y) = main(6)')
+    print("max palindrome = " , m , " from numbers ", x, " , ", y)
+
+    # print(list)
+
     print("--- %s seconds ---" % (time.time() - start_time))
